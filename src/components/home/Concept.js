8cfc3066ca42627a4dbdddec69bf06692a072f2d
Goh Kai Ling <108466713+KaiLing10@users.noreply.github.com>
2023-07-05 00:04:45 +0800

    update concept.js, clean code

--- src/components/home/Concept.js
+++ src/components/home/Concept.js
@@ -1,56 +1,101 @@
 import React from 'react'
-import ConceptImage from '../../assets/concept.jpg'
 import { useInView } from 'react-intersection-observer';
 import { motion } from 'framer-motion'
+import ConceptImage from '../../assets/concept.jpg'
 
-export default function Concept() {
+
+// Animation variants
+const fadeInText = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+};
+
+const sentence = {
+    hidden: { opacity: 1 },
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.05,
+        },
+    },
+}
+
+const letter = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+}
+
+// Text Animation Component
+const AnimatedText = ({ text }) => (
+    <>
+        {text.split("").map((char, index) => (
+            <motion.span key={char + "_" + index} variants={letter}>
+                {char}
+            </motion.span>
+        ))}
+    </>
+);
+
+// Main component
+const Concept = () => {
+
+    // Track element visibility
     // https://www.react-spring.dev/docs/utilities/use-in-view#usage
-    const [ref, inView] = useInView({
+    const [ref, inViewText] = useInView({
         threshold: 1,
     });
-
-    const [ref2, inView2] = useInView({
-        threshold: 0.8,
+    const [ref2, inViewImage] = useInView({
+        threshold: 0.7,
     });
 
-    const fadeInVariant = {
-        hidden: { opacity: 0, y: 20 },
-        visible: { opacity: 1, y: 0 },
-
-    };
-
     return (
-
         <div className={`bg-gray-100 h-2/3 pt-10 `}>
 
             <motion.div
                 className='m-auto pl-20 pb-6 text-green-800 text-xl md:text-2xl'
-                variants={fadeInVariant}
+                variants={fadeInText}
                 initial='hidden'
-                animate={inView ? 'visible' : 'hidden'}
+                animate={inViewText ? 'visible' : 'hidden'}
                 transition={{ ease: "easeInOut", duration: 0.8 }}
                 ref={ref}
             >
                 <p>Situated within the lush greenery of the surrounding hillsides</p>
-
             </motion.div>
-            <div className='grid grid-cols-5 '>
 
+
+            <div className='grid grid-cols-5 '>
+                {/* image */}
                 <motion.div
                     className='pl-20 col-span-3'
-                    variants={fadeInVariant}
-                    initial='hidden'
-                    animate={inView2 ? 'visible' : 'hidden'}
-                    transition={{ ease: "easeInOut", duration: 0.8 }}
-                    ref={ref2}>
+                    initial={{ opacity: 0 }}
+                    animate={inViewImage ? { opacity: 1 } : { opacity: 0 }}
+                    transition={{ ease: "easeInOut", duration: 1.5 }}
+                    ref={ref2}
+                >
                     <img src={ConceptImage} alt='condo_image' />
                 </motion.div>
-                <div className='col-span-2 p-10 pt-24 text-2xl md:text-3xl'>
-                    <p>It is an excellent heaven from the hustle and bustle of the city.</p>
-                </div>
+
+                {/* text content */}
+                <motion.div
+                    className="load-screen--message col-span-2 p-10 pt-24 text-2xl md:text-3xl"
+                    variants={sentence}
+                    initial="hidden"
+                    animate={inViewImage ? 'visible' : 'hidden'}
+                >
+                    <AnimatedText text="' It is an excellent heaven" variants={letter} />
+                    <br />
+                    <AnimatedText text="from the hustle and bustle" variants={letter} />
+                    <br />
+                    <AnimatedText text="of the city '" variants={letter} />
+
+                </motion.div>
+
             </div>
         </div>
 
 
     );
+
 }
+
+export default Concept
--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { motion } from "framer-motion";
 
 // https://wrongakram.notion.site/Framer-Motion-Basic-Animations-d464a00579144f5c86998731f7afba00
+// Animation variants
 const banner = {
   initial: {},
   animate: {
@@ -27,46 +28,26 @@ const letterAni = {
   },
 };
 
-
-const AnimatedLetters = ({ title }) => (
+// Text Animation Component
+const AnimatedText = ({ text }) => (
   <motion.div
     className="relative tracking-wider inline-block whitespace-nowrap "
     variants={banner}
     initial="initial"
     animate="animate"
   >
-    {[...title].map((letter) => (
+    {text.split("").map((char, index) => (
+
       <motion.div
         className="relative tracking-wider inline-block whitespace-nowrap "
-        variants={letterAni}
-      >
-        {letter}
+        key={char + "_" + index} variants={letterAni}>
+        {char}
+
       </motion.div>
     ))}
+
   </motion.div>
 );
-
-
-const BannerRowTop = ({ title }) => {
-  return (
-    <div className={"banner-row"}>
-      <div className="row-col text-xl md:text-3xl lg:text-4xl text-left">
-        <AnimatedLetters title={title} />
-      </div>
-    </div>
-  );
-};
-
-
-const BannerRowCenter = ({ title }) => {
-  return (
-    <div className={`banner-row marquee  "animate" text-4xl md:text-7xl lg:text-8xl text-left}`}>
-      <div className="marquee__inner">
-        <AnimatedLetters title={title} />
-      </div>
-    </div>
-  );
-};
 
 
 const Hero = () => {
@@ -92,8 +73,13 @@ const Hero = () => {
         <div className=' text-white'>
           <motion.div className='banner' variants={banner}>
 
-            <BannerRowTop title={"Welcome to"} />
-            <BannerRowCenter title={"Parkhill Residence"} />
+            <div className="row-col text-xl md:text-3xl lg:text-4xl text-left">
+              <AnimatedText text={"Welcome\u00A0to"} />
+            </div>
+
+            <div className="text-4xl md:text-7xl lg:text-8xl text-left">
+              <AnimatedText text={"Parkhill\u00A0Residence"} />
+            </div>
 
             <motion.div
               initial={{ opacity: 0, y: 30 }}
@@ -120,16 +106,15 @@ const Hero = () => {
         transition={{ duration: 1, delay: 1 }}
         className="absolute z-10 top-0 left-0 bg-black/30 w-full h-screen">
       </motion.div>
+
       {/* Image */}
-
-
       <img
         src={process.env.PUBLIC_URL + `/loader/loader2.jpeg`}
         alt='condo_image_background'
         className={`bg-white fixed w-full h-screen object-cover `}
         style={{
           transform: `scale(${1 + Math.min(scrollPosition / window.innerHeight, 1) * 0.5})`,
-          opacity: 1 - Math.min(scrollPosition / window.innerHeight, 1)*0.8,
+          opacity: 1 - Math.min(scrollPosition / window.innerHeight, 1) * 0.8,
         }}
       />
     </div>
